0875654bdf25a331fb4ff79c5b58ec7fa469f1fd
RamtinKosari <ramtinkosari@gmail.com>
2024-10-26 15:53:28 +0330

    [#REFACTOR] Add Rotor Handler

---
+++ Rotor/RotorHandler.py
@@ -0,0 +1,29 @@
+# - Import RKEL Configurations
+from Configs import *
+
+# - RotorHandler Class Definition
+class RotorHandler:
+    # - Method to Initialize Rotor Handler
+    def __init__(self, pin):
+        # - Setup GPIO Mode
+        GPIO.setmode(GPIO.BCM)
+        # - Set Rotor Pin
+        self.pin = pin
+        # - Set PWM Rotor
+        self.PWM_ROTOR = None
+        # - Setup GPIO Pin
+        GPIO.setup(pin, GPIO.OUT)
+        # - Set LED State
+        GPIO.output(pin, GPIO.LOW)
+        # - Initialize PWM Rotor
+        self.PWM_ROTOR = GPIO.PWM(self.pin, 100)
+        # - Start PWM Rotor at 0 Duty Cycle
+        self.PWM_ROTOR.start(0)
+        # - Show Log
+        printRKEL(RKEL_LABEL, SUCCESS, "Rotor Pin {}{}{} Initialized".format(LIGHT_INFO, pin, RESET), force = True)
+    # - Method to Power Rotor
+    def power(self, pin, percentage: int):
+        # - Show Log
+        printRKEL(RKEL_LABEL, "Powering Rotor at {}{}{}% ...".format(LIGHT_INFO, percentage, RESET), force = True)
+        # - Set Duty Cycle
+        self.PWM_ROTOR.ChangeDutyCycle(100)
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from LED.Animation import *
+from Rotor.RotorHandler import *
 
 if __name__ == '__main__':
-    # - Initialize Animation
-    animation = Animation([4, 17, 27, 22, 10, 9, 11, 0, 5, 6, 13, 19, 26])
-    # - Blink Animation
-    # animation.blink(0.04, 1, 'blink-array', AnimationArrays.Blink.Scan)
-    animation.fade(0.04, 1, 'fade-out', AnimationArrays.Fade.Wave)
-    # - Clear LEDs
-    animation.clear()
+    # # - Initialize Animation
+    # animation = Animation([4, 17, 27, 22, 10, 9, 11, 0, 5, 6, 13, 19, 26])
+    # # - Blink Animation
+    # # animation.blink(0.04, 1, 'blink-array', AnimationArrays.Blink.Scan)
+    # animation.fade(0.04, 1, 'fade-out', AnimationArrays.Fade.Wave)
+    # # - Clear LEDs
+    # animation.clear()
+
+
+    # - Initialize Rotor Handler
+    rotor = RotorHandler()
+    rotor.power(20)
