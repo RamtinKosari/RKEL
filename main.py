6d6b8fd86a262ae9b8b75e60979b3a960047c04d
RamtinKosari <ramtinkosari@gmail.com>
2024-10-26 13:13:54 +0330

    [#REFACTOR] Add Blink Scan Animation Arrays

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     # - Initialize Animation
     animation = Animation([4, 17, 27, 22, 10, 9, 11, 0, 5, 6, 13, 19, 26])
     # - Blink Animation
-    animation.blink(0.04, 1, 'blink-array', AnimationArrays.Blink.Wave)
+    animation.blink(0.04, 2, 'blink-array', AnimationArrays.Blink.Scan)
     # - Clear LEDs
     animation.clear()
