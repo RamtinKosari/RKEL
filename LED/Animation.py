1445e7104c1a76259d8d8ab60e238fa416dcfd04
RamtinKosari <ramtinkosari@gmail.com>
2024-10-26 12:26:46 +0330

    [#REFACTOR] Add Serial and Parallel Blinking Animation

--- LED/Animation.py
+++ LED/Animation.py
@@ -14,12 +14,25 @@ class Animation:
         # - Cleanup GPIO Settings
         GPIO.cleanup()
     # - Blink Animation
-    def blink(self, delay = 0.001, times = 5):
+    def blink(self, delay = 0.001, times = 5, mode = 'serial'):
         for _ in range(times):
-            for pin in self.leds.pins:
-                self.leds.on(pin)
+            # - Check Mode
+            if mode == 'serial':
+                # - Serial Blinking
+                for pin in self.leds.pins:
+                    self.leds.on(pin)
+                    time.sleep(delay)
+                    self.leds.off(pin)
+                    time.sleep(delay)
+            elif mode == 'parallel':
+                # - Parallel Blinking
+                for pin in self.leds.pins:
+                    self.leds.on(pin)
                 time.sleep(delay)
-                self.leds.off(pin)
+                for pin in self.leds.pins:
+                    self.leds.off(pin)
                 time.sleep(delay)
+            else:
+                pass
         # - Clear LEDs
         self.clear()
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     # - Initialize Animation
     animation = Animation([4, 17, 27, 22, 10, 9, 11, 0, 5, 6, 13, 19, 26])
     # - Blink Animation
-    animation.blink(0.1, 3)
+    animation.blink(0.1, 3, 'serial')
