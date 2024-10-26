5c52d20b43e86336403b372f6ef4c557503059b8
RamtinKosari <ramtinkosari@gmail.com>
2024-10-26 13:16:39 +0330

    [#REFACTOR] Add Reverse Flag to Reverse Blink Array

--- LED/Animation.py
+++ LED/Animation.py
@@ -17,22 +17,18 @@ class Animation:
         # - Cleanup GPIO Settings
         GPIO.cleanup()
     # - Blink Animation
-    def blink(self, delay = 0.001, times = 5, mode = 'serial', blink_array = [[0, 2, 4, 6], [1, 3, 5, 7]]):
+    def blink(self, delay = 0.001, times = 5, mode = 'serial', blink_array = [[0, 2, 4, 6], [1, 3, 5, 7]], array_reverse = False):
         # - Show Log
         printRKEL(RKEL_LABEL, "Blinking LEDs for {}{}{} Times with {}{}{} Seconds Delay in {}{}{} Mode ...".format(LIGHT_INFO, times, RESET, LIGHT_INFO, delay, RESET, LIGHT_INFO, mode, RESET), force = True)
+        # - Check Reverse
+        if array_reverse:
+            blink_array = blink_array.reverse()
         # - Blink LEDs
         for _ in range(times):
             # - Check Mode
             if mode == 'serial':
                 # - Serial Blinking
                 for pin in self.leds.pins:
-                    self.leds.on(pin)
-                    time.sleep(delay)
-                    self.leds.off(pin)
-                    time.sleep(delay)
-            elif mode == 'serial-reverse':
-                # - Serial Reverse Blinking
-                for pin in reversed(self.leds.pins):
                     self.leds.on(pin)
                     time.sleep(delay)
                     self.leds.off(pin)
--- main.py
+++ main.py
@@ -5,5 +5,6 @@ if __name__ == '__main__':
     animation = Animation([4, 17, 27, 22, 10, 9, 11, 0, 5, 6, 13, 19, 26])
     # - Blink Animation
     animation.blink(0.04, 2, 'blink-array', AnimationArrays.Blink.Scan)
+    animation.blink(0.04, 2, 'blink-array', AnimationArrays.Blink.Scan, True)
     # - Clear LEDs
     animation.clear()
