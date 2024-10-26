4b4b150ee79618fcec452e073fa8bb17d239265c
RamtinKosari <ramtinkosari@gmail.com>
2024-10-26 12:18:45 +0330

    [#REFACTOR] Add Blink Animation

--- LED/Animation.py
+++ LED/Animation.py
@@ -1,12 +1,17 @@
 # - Include LEDs Handler Module
-from .LEDsHandler import *
+from .LEDHandler import *
 
 # - Animation Class Definition
 class Animation:
     # - Method to Initialize Animation
     def __init__(self, pins):
-        self.leds = LEDsHandler(pins)
+        self.leds = LEDHandler(pins)
     # - Blink Animation
     @staticmethod
-    def blink(delay = 0.001):
-        pass
+    def blink(self, delay = 0.001, times = 5):
+        for _ in range(times):
+            for pin in self.leds.pins:
+                self.leds.on(pin)
+                time.sleep(delay)
+                self.leds.off(pin)
+                time.sleep(delay)
--- LED/LEDHandler.py
+++ LED/LEDHandler.py
@@ -1,15 +1,25 @@
 # - Import RKEL Configurations
 from Configs import *
 
-# - LED Handler Class Definition
+# - LEDs Handler Class Definition
 class LEDHandler:
-    # - Method to Initialize LED Handler
-    def __init__(self, pin, model = 'Raspberry Pi'):
-        # - Set Pin Number
-        self.pin = pin
+    def __init__(self, pins):
+        # - Setup GPIO Mode
+        GPIO.setmode(GPIO.BCM)
+        # - Set LED Pins
+        self.pins = pins
+        # - Initialize GPIO Pins
+        for pin in self.pins:
+            # - Setup GPIO Pin
+            GPIO.setup(pin, GPIO.OUT)
+            # - Set LED State
+            GPIO.output(self.pin, GPIO.LOW)
+            # - Show Log
+            printRKEL(RKEL_LABEL, SUCCESS, "LED Pin {}{}{} Initialized".format(LIGHT_INFO, pin, RESET), force = True)
     # - Method to Turn LED On
-    def on(self):
-        GPIO.output(self.pin, GPIO.HIGH)
+    def on(self, pin):
+        GPIO.output(pin, GPIO.HIGH)
     # - Method to Turn LED Off
-    def off(self):
-        GPIO.output(self.pin, GPIO.LOW)
+    def off(self, pin):
+        GPIO.output(pin, GPIO.LOW)
+            
