2253c010d8783731b15d5176f64715de4711fa51
RamtinKosari <ramtinkosari@gmail.com>
2024-10-26 12:22:15 +0330

    [#REFACTOR] Add Method to Clear LEDs

--- LED/Animation.py
+++ LED/Animation.py
@@ -6,6 +6,13 @@ class Animation:
     # - Method to Initialize Animation
     def __init__(self, pins):
         self.leds = LEDHandler(pins)
+    # - Clear LEDs
+    def clear(self):
+        # - Turn Off All LEDs
+        for pin in self.leds.pins:
+            self.leds.off(pin)
+        # - Cleanup GPIO Settings
+        GPIO.cleanup()
     # - Blink Animation
     def blink(self, delay = 0.001, times = 5):
         for _ in range(times):
@@ -14,3 +21,5 @@ class Animation:
                 time.sleep(delay)
                 self.leds.off(pin)
                 time.sleep(delay)
+        # - Clear LEDs
+        self.clear()
