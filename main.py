f2e2a3b469dea12f6c47eed7c33e174c422a13eb
RamtinKosari <ramtinkosari@gmail.com>
2024-10-26 13:09:55 +0330

    [#REFACTOR] Add Blink Wave Animation Arrays

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     # - Initialize Animation
     animation = Animation([4, 17, 27, 22, 10, 9, 11, 0, 5, 6, 13, 19, 26])
     # - Blink Animation
-    animation.blink(0.04, 1, 'blink-array', AnimationArrays.Blink.Incremental)
+    animation.blink(0.04, 1, 'blink-array', AnimationArrays.Blink.Wave)
     # - Clear LEDs
     animation.clear()
